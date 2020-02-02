ae99c2401faf56abe7915e52af9715baf95ce6a4
Diego Costa <diegocostacmp@gmail.com>
2020-02-02 20:06:11 -0300

    setup method managerCity to test

--- api.py
+++ api.py
@@ -1,2 +1,18 @@
-from flask import flask
-from flas
+from flask import Flask
+from flask import Flask, request
+# from flask_restfull import Resource, Api
+import flask_restful as restfull
+
+
+app = Flask(__name__)
+api = restfull.Api(app)
+
+# define class name
+class managerCity(restfull.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(managerCity, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
